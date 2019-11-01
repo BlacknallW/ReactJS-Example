800a513bdf9974c881ef4f65827ca1219c2b8154
William Blacknall <williamblacknall@gmail.com>
2019-11-01 10:40:49 -0400

    Users able to add and remove items from TodoList with unique ids

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import './App.css';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
-
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 class App extends React.Component {
 state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id:uuid.v4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       },
     ]
   }
-
+// Toggle Complete
   markComplete = (id) => {
     this.setState({todos: this.state.todos.map(todo => {
       if(todo.id === id ) {
@@ -32,11 +34,31 @@ state = {
     })
   })
   }
+
+//Delete Todo
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+}
+
+
+//Add Todo
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos, newTodo]})
+}
 render() {
   console.log(this.state.todos)
   return (
     <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      <div className="container">
+        <Header />
+        <AddTodo addTodo={this.addTodo}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
       </div>
     );
   }
